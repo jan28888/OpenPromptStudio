9e9ae9d091ac4411a5f497632fe962e4b75a6fd4
jan28888 <565453078@qq.com>
2025-09-14 19:32:26 +0800

    本地build

--- data/build.js
+++ data/build.js
@@ -1,6 +1,6 @@
 import csv from "csvtojson"
 import fs from "fs"
-import { fromNotion } from "./src/notion/fromNotion.js"
+// import { fromNotion } from "./src/notion/fromNotion.js"
 import localCommandDesc from "./src/localCommandDesc.js"
 
 const __dirname = new URL(".", import.meta.url).pathname
@@ -8,8 +8,8 @@ const __dirname = new URL(".", import.meta.url).pathname
 let localPromptDefineMap = {}
 
 // Add notion database https://www.notion.so/moonvy/5ac19c115d11488f95847c9e2d789dff?v=5ce9b783b4504c23bb7b492aa70c1cfc
-let notionPromptDescMap = await fromNotion()
-Object.assign(localPromptDefineMap, notionPromptDescMap)
+// let notionPromptDescMap = await fromNotion()
+// Object.assign(localPromptDefineMap, notionPromptDescMap)
 
 // Add src/dict/*.csv
 let pathLang = `${__dirname}src/dict`
